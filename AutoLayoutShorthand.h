9ada7b399943f0a51281ba749c72239bb1d9188a
rentzsch <jwr.git@redshed.net>
2013-06-18 22:45:29 -0500

    Re-unify UIView and NSView implementations.

--- AutoLayoutShorthand.h
+++ AutoLayoutShorthand.h
@@ -1,12 +1,38 @@
-// AutoLayoutShorthand.h semver:0.2
+// AutoLayoutShorthand.h semver:0.3
 //   Copyright (c) 2013 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
 //   Some rights reserved: http://opensource.org/licenses/mit
 //   https://github.com/rentzsch/AutoLayoutShorthand
 
-#import "AutoLayoutConstants.h"
-
 #ifdef __IPHONE_OS_VERSION_MIN_REQUIRED
-#import "UIView+AutoLayoutShorthand.h"
+    #import <UIKit/UIKit.h>
+    #define ALSView                    UIView
+    #define ALSLayoutPriority          UILayoutPriority
+    #define ALSLayoutPriorityRequired  UILayoutPriorityRequired
 #elif TARGET_OS_MAC
-#import "NSView+AutoLayoutShorthand.h"
+    #import <Cocoa/Cocoa.h>
+    #define ALSView                    NSView
+    #define ALSLayoutPriority          NSLayoutPriority
+    #define ALSLayoutPriorityRequired  NSLayoutPriorityRequired
 #endif
+
+@interface ALSView (AutoLayoutShorthand)
+- (id)als_left;
+- (id)als_right;
+- (id)als_top;
+- (id)als_bottom;
+- (id)als_leading;
+- (id)als_trailing;
+- (id)als_width;
+- (id)als_height;
+- (id)als_centerX;
+- (id)als_centerY;
+- (id)als_baseline;
+
+- (void)als_addConstraints:(NSDictionary*)constraints;
+@end
+
+extern NSString * const als_view;
+extern NSString * const als_superview;
+extern NSString * const als_multiplier;
+extern NSString * const als_constant;
+extern NSString * const als_priority;
--- AutoLayoutShorthand.m
+++ AutoLayoutShorthand.m
@@ -1,58 +1,41 @@
-// UIView+AutoLayoutShorthand.m semver:0.2
+// AutoLayoutShorthand.m semver:0.3
 //   Copyright (c) 2013 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
 //   Some rights reserved: http://opensource.org/licenses/mit
 //   https://github.com/rentzsch/AutoLayoutShorthand
 
-#import "UIView+AutoLayoutShorthand.h"
-#import "AutoLayoutConstants.h"
+#import "AutoLayoutShorthand.h"
 
 @interface ALSViewAttr : NSObject
-@property(nonatomic, strong)  UIView           *view;
-@property(nonatomic, assign)  NSLayoutAttribute attr;
+@property(nonatomic, strong)  ALSView            *view;
+@property(nonatomic, assign)  NSLayoutAttribute  attr;
 
-+ (instancetype)viewAttrWithView:(UIView *)view attr:(NSLayoutAttribute)attr;
++ (instancetype)viewAttrWithView:(ALSView*)view attr:(NSLayoutAttribute)attr;
 @end
 
-@implementation ALSViewAttr
+@implementation ALSView (AutoLayoutShorthand)
 
-+ (instancetype)viewAttrWithView:(UIView *)view attr:(NSLayoutAttribute)attr
-{
-    ALSViewAttr *result = [[ALSViewAttr alloc] init];
-
-    result.view = view;
-    result.attr = attr;
-    return result;
-}
-
-@end
-
-@implementation UIView (AutoLayoutShorthand)
-
-+ (NSLayoutAttribute)_jr_parseLayoutAttributeName:(NSString *)key
-{
-    static NSDictionary   *attrValueForName = nil;
++ (NSLayoutAttribute)_jr_parseLayoutAttributeName:(NSString*)key {
+    static NSDictionary *attrValueForName = nil;
     static dispatch_once_t onceToken;
-
     dispatch_once(&onceToken, ^{
         attrValueForName =
         @{
-          @"left" : @(NSLayoutAttributeLeft),
-          @"right" : @(NSLayoutAttributeRight),
-          @"top" : @(NSLayoutAttributeTop),
-          @"bottom" : @(NSLayoutAttributeBottom),
-          @"leading" : @(NSLayoutAttributeLeading),
-          @"trailing" : @(NSLayoutAttributeTrailing),
-          @"width" : @(NSLayoutAttributeWidth),
-          @"height" : @(NSLayoutAttributeHeight),
-          @"centerX" : @(NSLayoutAttributeCenterX),
-          @"centerY" : @(NSLayoutAttributeCenterY),
-          @"baseline" : @(NSLayoutAttributeBaseline),
+          @"left":      @(NSLayoutAttributeLeft),
+          @"right":     @(NSLayoutAttributeRight),
+          @"top":       @(NSLayoutAttributeTop),
+          @"bottom":    @(NSLayoutAttributeBottom),
+          @"leading":   @(NSLayoutAttributeLeading),
+          @"trailing":  @(NSLayoutAttributeTrailing),
+          @"width":     @(NSLayoutAttributeWidth),
+          @"height":    @(NSLayoutAttributeHeight),
+          @"centerX":   @(NSLayoutAttributeCenterX),
+          @"centerY":   @(NSLayoutAttributeCenterY),
+          @"baseline":  @(NSLayoutAttributeBaseline),
           };
     });
-
-    NSString *attrName     = [key componentsSeparatedByString:@" "][0];
+    
+    NSString *attrName = [key componentsSeparatedByString:@" "][0];
     NSNumber *attrValueObj = attrValueForName[attrName];
-
     if (attrValueObj) {
         return [attrValueObj integerValue];
     } else {
@@ -61,23 +44,20 @@
     }
 }
 
-+ (NSLayoutRelation)_jr_parseLayoutRelation:(NSString *)key
-{
-    static NSDictionary   *relationValueForName = nil;
++ (NSLayoutRelation)_jr_parseLayoutRelation:(NSString*)key {
+    static NSDictionary *relationValueForName = nil;
     static dispatch_once_t onceToken;
-
     dispatch_once(&onceToken, ^{
         relationValueForName =
         @{
-          @"==" : @(NSLayoutRelationEqual),
-          @">=" : @(NSLayoutRelationGreaterThanOrEqual),
-          @"<=" : @(NSLayoutRelationLessThanOrEqual),
+          @"==":  @(NSLayoutRelationEqual),
+          @">=":  @(NSLayoutRelationGreaterThanOrEqual),
+          @"<=":  @(NSLayoutRelationLessThanOrEqual),
           };
     });
-
-    NSString *relationName     = [key componentsSeparatedByString:@" "][1];
+    
+    NSString *relationName = [key componentsSeparatedByString:@" "][1];
     NSNumber *relationValueObj = relationValueForName[relationName];
-
     if (relationValueObj) {
         return [relationValueObj integerValue];
     } else {
@@ -86,31 +66,28 @@
     }
 }
 
-- (void)als_addConstraints:(NSDictionary *)constraints
-{
+- (void)als_addConstraints:(NSDictionary*)constraints {
     for (NSString *constraintKey in constraints) {
-        UIView           *firstItem       = self;
-        NSLayoutAttribute firstAttribute  = [UIView _jr_parseLayoutAttributeName:constraintKey];
-        NSLayoutRelation  relation        = [UIView _jr_parseLayoutRelation:constraintKey];
-        UIView           *secondItem      = nil;
+        ALSView *firstItem = self;
+        NSLayoutAttribute firstAttribute = [ALSView _jr_parseLayoutAttributeName:constraintKey];
+        NSLayoutRelation relation = [ALSView _jr_parseLayoutRelation:constraintKey];
+        ALSView *secondItem = nil;
         NSLayoutAttribute secondAttribute = NSLayoutAttributeNotAnAttribute;
-        CGFloat           multiplier      = 1.0;
-        CGFloat           constant        = 0.0;
-        UILayoutPriority  priority        = UILayoutPriorityRequired;
-
+        CGFloat multiplier = 1.0;
+        CGFloat constant = 0.0;
+        ALSLayoutPriority priority = ALSLayoutPriorityRequired;
+        
         id constraintValue = constraints[constraintKey];
-
         if ([constraintValue isKindOfClass:[ALSViewAttr class]]) {
             ALSViewAttr *viewAttribute = constraintValue;
-            secondItem      = viewAttribute.view;
+            secondItem = viewAttribute.view;
             secondAttribute = viewAttribute.attr;
         } else if ([constraintValue isKindOfClass:[NSDictionary class]]) {
             NSDictionary *constraintValueNumberDict = constraintValue;
-
             if (constraintValueNumberDict[als_view]) {
                 if ([constraintValueNumberDict[als_view] isKindOfClass:[NSString class]]) {
                     if ([constraintValueNumberDict[als_view] isEqualToString:als_superview]) {
-                        secondItem      = firstItem.superview;
+                        secondItem = firstItem.superview;
                         secondAttribute = firstAttribute;
                     } else {
                         NSAssert3(NO,
@@ -122,27 +99,24 @@
                     }
                 } else {
                     ALSViewAttr *viewAttribute = constraintValueNumberDict[als_view];
-                    secondItem      = viewAttribute.view;
+                    secondItem = viewAttribute.view;
                     secondAttribute = viewAttribute.attr;
                 }
             }
-
             if (constraintValueNumberDict[als_multiplier]) {
                 multiplier = [constraintValueNumberDict[als_multiplier] doubleValue];
             }
-
             if (constraintValueNumberDict[als_constant]) {
                 constant = [constraintValueNumberDict[als_constant] doubleValue];
             }
-
             if (constraintValueNumberDict[als_priority]) {
-                priority = [constraintValueNumberDict[als_priority] doubleValue];
+                constant = [constraintValueNumberDict[als_priority] doubleValue];
             }
         } else if ([constraintValue isKindOfClass:[NSNumber class]]) {
             constant = [constraintValue doubleValue];
         } else if ([constraintValue isKindOfClass:[NSString class]]) {
             if ([constraintValue isEqualToString:als_superview]) {
-                secondItem      = firstItem.superview;
+                secondItem = firstItem.superview;
                 secondAttribute = firstAttribute;
             } else {
                 NSAssert2(NO,
@@ -159,7 +133,7 @@
                       constraintValue
                       );
         }
-
+        
         NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:firstItem
                                                                       attribute:firstAttribute
                                                                       relatedBy:relation
@@ -168,26 +142,23 @@
                                                                      multiplier:multiplier
                                                                        constant:constant];
         constraint.priority = priority;
-
+        
+        
         if (secondItem) {
-            UIView *closestCommonSuperview = nil;
-
-            UIView *rhsViewItr = self.superview;
-
+            ALSView *closestCommonSuperview = nil;
+            
+            ALSView *rhsViewItr = self.superview;
             while (!closestCommonSuperview && rhsViewItr) {
-                UIView *lhsViewItr = secondItem;
-
+                ALSView *lhsViewItr = secondItem;
                 while (!closestCommonSuperview && lhsViewItr) {
                     if (rhsViewItr == lhsViewItr) {
                         closestCommonSuperview = rhsViewItr;
                     }
-
                     lhsViewItr = lhsViewItr.superview;
                 }
-
                 rhsViewItr = rhsViewItr.superview;
             }
-
+            
             NSAssert2(closestCommonSuperview,
                       @"couldn't find a common superview for %@ and %@",
                       firstItem,
@@ -199,59 +170,33 @@
     }
 }
 
-- (id)als_left
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeLeft];
-}
+- (id)als_left      { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeLeft];      }
+- (id)als_right     { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeRight];     }
+- (id)als_top       { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeTop];       }
+- (id)als_bottom    { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeBottom];    }
+- (id)als_leading   { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeLeading];   }
+- (id)als_trailing  { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeTrailing];  }
+- (id)als_width     { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeWidth];     }
+- (id)als_height    { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeHeight];    }
+- (id)als_centerX   { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeCenterX];   }
+- (id)als_centerY   { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeCenterY];   }
+- (id)als_baseline  { return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeBaseline];  }
 
-- (id)als_right
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeRight];
-}
+@end
 
-- (id)als_top
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeTop];
-}
+@implementation ALSViewAttr
 
-- (id)als_bottom
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeBottom];
-}
-
-- (id)als_leading
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeLeading];
-}
-
-- (id)als_trailing
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeTrailing];
-}
-
-- (id)als_width
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeWidth];
-}
-
-- (id)als_height
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeHeight];
-}
-
-- (id)als_centerX
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeCenterX];
-}
-
-- (id)als_centerY
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeCenterY];
-}
-
-- (id)als_baseline
-{
-    return [ALSViewAttr viewAttrWithView:self attr:NSLayoutAttributeBaseline];
++ (instancetype)viewAttrWithView:(ALSView*)view attr:(NSLayoutAttribute)attr {
+    ALSViewAttr *result = [[ALSViewAttr alloc] init];
+    result.view = view;
+    result.attr = attr;
+    return result;
 }
 
 @end
+
+NSString * const als_view        = @"als_view";
+NSString * const als_superview   = @"als_superview";
+NSString * const als_constant    = @"als_constant";
+NSString * const als_multiplier  = @"als_multiplier";
+NSString * const als_priority    = @"als_priority";
